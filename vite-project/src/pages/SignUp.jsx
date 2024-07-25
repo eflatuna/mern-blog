250646a0c97d38e0b7145e0414c8824d6cd856a8
esra <esrabaskaya84@gmail.com>
2024-07-25 23:40:09 +0200

    add functionality to the sign up page

--- vite-project/src/pages/SignUp.jsx
+++ vite-project/src/pages/SignUp.jsx
@@ -1,26 +1,45 @@
 "use strict";
-import { Button, Label, TextInput } from "flowbite-react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React from "react";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
 	const [formData, setFormData] = useState({});
-
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 	const handleChange = (e) => {
-		setFormData({ ...formData, [e.target.id]: e.target.value });
+		setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
 	};
 	// console.log(formData);
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		if (!formData.username || !formData.email || !formData.password) {
+			return setErrorMessage("Please fill out all fields!");
+		}
+
 		try {
+			setLoading(true);
+			setErrorMessage(null);
 			const res = await fetch("/api/auth/signup", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
 				body: JSON.stringify(formData),
 			});
 			const data = await res.json();
-		} catch (error) {}
+
+			if (data.success === false) {
+				return setErrorMessage(data.message);
+			}
+			setLoading(false);
+			if (res.ok) {
+				navigate("/sign-in");
+			}
+		} catch (error) {
+			setErrorMessage(error.message);
+			setLoading(false);
+		}
 	};
 
 	return (
@@ -70,8 +89,19 @@ const SignUp = () => {
 								onChange={handleChange}
 							/>
 						</div>
-						<Button gradientDuoTone="purpleToPink" type="submit">
-							Sign Up
+						<Button
+							gradientDuoTone="purpleToPink"
+							type="submit"
+							disabled={loading}
+						>
+							{loading ? (
+								<>
+									<Spinner size="sm" />
+									<span className="'pl-3">Loading...</span>
+								</>
+							) : (
+								"Sign-up"
+							)}
 						</Button>
 					</form>
 					<div className="flex gap-2 text-sm mt-5">
@@ -80,6 +110,11 @@ const SignUp = () => {
 							Sign In
 						</Link>
 					</div>
+					{errorMessage && (
+						<Alert className="mt-5" color="failure">
+							{errorMessage}
+						</Alert>
+					)}
 				</div>
 			</div>
 		</div>
