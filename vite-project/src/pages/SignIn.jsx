3ce55306cad96fe67dfc823594767e41b620dfae
esra <esrabaskaya84@gmail.com>
2024-07-27 23:38:14 +0200

    add redux toolkit

--- vite-project/src/pages/SignIn.jsx
+++ vite-project/src/pages/SignIn.jsx
@@ -3,11 +3,17 @@ import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+	signInStart,
+	signInSuccess,
+	signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const SignIn = () => {
 	const [formData, setFormData] = useState({});
-	const [errorMessage, setErrorMessage] = useState(null);
-	const [loading, setLoading] = useState(false);
+	const { loading, error: errorMessage } = useSelector((state) => state.user);
+	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const handleChange = (e) => {
 		setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -16,12 +22,11 @@ const SignIn = () => {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		if (!formData.email || !formData.password) {
-			return setErrorMessage("Please fill out all fields!");
+			return dispatch(signInFailure("Please fill all the fields"));
 		}
 
 		try {
-			setLoading(true);
-			setErrorMessage(null);
+			dispatch(signInStart());
 			const res = await fetch("/api/auth/signin", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
@@ -30,15 +35,15 @@ const SignIn = () => {
 			const data = await res.json();
 
 			if (data.success === false) {
-				return setErrorMessage(data.message);
+				dispatch(signInFailure(data.message));
 			}
-			setLoading(false);
+
 			if (res.ok) {
+				dispatch(signInSuccess(data));
 				navigate("/");
 			}
 		} catch (error) {
-			setErrorMessage(error.message);
-			setLoading(false);
+			dispatch(signInFailure(error.message));
 		}
 	};
 
